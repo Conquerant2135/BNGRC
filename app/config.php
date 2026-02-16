2c635d085d6e1ace05f411bd4aa8aef2172f9c6b
Xioandryan69 <andriannyavo9@gmail.com>
2026-02-16 20:29:12 +0300

    config.php root

--- app/config.php
+++ app/config.php
@@ -4,8 +4,10 @@
 
 define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'bngrc');
-define('DB_USER', 'ETU004367');
-define('DB_PASS', 'uSbxQVRP');
+// define('DB_USER', 'ETU004367');
+// define('DB_PASS', 'uSbxQVRP');
+ define('DB_USER', 'root');
+ define('DB_PASS', '');
 define('DB_CHARSET' , 'utf8mb4');
 
 $baseUrl = rtrim(str_replace('\\', '/', dirname(path: $_SERVER['SCRIPT_NAME'] ?? '/')), '/');

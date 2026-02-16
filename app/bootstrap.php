00f37a6204c84057c6b64085c7aa7f4f76a9dde2
Conquerant2135 <manourakotoarinia@gmail.com>
2026-02-16 14:00:15 +0300

    fix: passage de postgres a mysql

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/config.php';
 
 Flight::register('db', 'PDO', array(
-    "psql:host=" . DB_HOST . ";dbname=" . DB_NAME . 
+    "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET,
     DB_USER,
     DB_PASS,
     array(

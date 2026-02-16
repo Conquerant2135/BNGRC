fa3847b5537e772c9a2588d236b44c6ab730453a
Xioandryan69 <andriannyavo9@gmail.com>
2026-02-16 14:28:38 +0300

    add data.sql create table ville schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -6,3 +6,11 @@ CREATE TABLE bgnrc_region (
     nom VARCHAR(50) NOT NULL
 );
 
+CREATE TABLE bgnrc_ville (
+    id_ville INT AUTO_INCREMENT PRIMARY KEY,
+    id_region INT,
+    nom_ville VARCHAR(100) NOT NULL,
+    nb_sinistres INT DEFAULT 0,
+    CONSTRAINT chk_sinistres CHECK (nb_sinistres >= 0),
+    CONSTRAINT fk_region FOREIGN KEY (id_region) REFERENCES bgnrc_region(id) 
+);

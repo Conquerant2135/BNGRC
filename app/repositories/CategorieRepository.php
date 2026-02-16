b9eb4c7fed6b9310aa51c0a43ba0bf30299e207f
Conquerant2135 <manourakotoarinia@gmail.com>
2026-02-16 19:01:21 +0300

    idk also

--- app/repositories/CategorieRepository.php
+++ app/repositories/CategorieRepository.php
@@ -12,6 +12,6 @@ class CategorieRepository
     public function all(): array
     {
         $stmt = $this->pdo->query("SELECT id, nom FROM bngrc_categorie ORDER BY nom");
-        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }

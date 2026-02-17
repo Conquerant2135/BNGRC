f479beb12a0fa572b5a081c5fb892acf869bb757
Conquerant2135 <manourakotoarinia@gmail.com>
2026-02-17 09:56:45 +0300

    add:  erreur lors d'insertion de truc similaire
    ajout de la situation de gestion des erreures ou si l'achat similaire est deja present ca fait une erreur

--- public/index.php
+++ public/index.php
@@ -2,4 +2,8 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../app/bootstrap.php';
 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+
 Flight::start();
